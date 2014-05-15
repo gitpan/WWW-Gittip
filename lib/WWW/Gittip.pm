385b021e30ad96fa88368cd030a433d68142cd82
גאבור סבו - Gábor Szabó <gabor@pti.co.il>
2014-05-15 20:01:35 +0000

    Import of SZABGAB/WWW-Gittip-0.02 from CPAN.
    gitpan-cpan-distribution: WWW-Gittip
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         SZABGAB/WWW-Gittip-0.02.tar.gz
    gitpan-cpan-author:       SZABGAB
    gitpan-cpan-maturity:     released

--- lib/WWW/Gittip.pm
+++ lib/WWW/Gittip.pm
@@ -5,7 +5,7 @@ use warnings;
 use LWP::Simple qw(get);
 use JSON qw(from_json);
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 NAME
 
@@ -16,13 +16,13 @@ WWW::Gittip - Implementing the Gittip API more or less
   use WWW::Gittip;
   my $charts = WWW::Gittip->charts;
 
-  my $charts = WWW::Gittip->charts;
+  my $user_charts = WWW::Gittip->user_charts('szabgab');
 
 =head1 DESCIPTION
 
 The API docs of Gittp: L<https://github.com/gittip/www.gittip.com#api>
 
-When necessary, you can get an API key from your account on Gittip at L<https://www.gittip.com/szabgab/account/>
+When necessary, you can get an API key from your account on Gittip at L<https://www.gittip.com/about/me/account>
 
 =head2 charts
 
@@ -43,9 +43,35 @@ Each element in the array has the following fields:
 
 
 sub charts {
-	my $url = 'https://www.gittip.com/about/charts.json';
-	my $charts = get $url;
-	return from_json $charts;
+	my $url = "https://www.gittip.com/about/charts.json";
+	return _get($url);
+}
+
+=head2 user_charts
+
+   WWW::Gittip->user_charts(USERNAME);
+
+Returns an array referene from /%username/charts.json
+Each element in the array has the following fields:
+
+   {
+     'date'     => '2012-06-08',
+     'npatrons' => 0,
+     'receipts' => '0',
+     'ts_start' => '2012-06-08T12:02:45.182409+00:00'
+   }
+
+
+=cut
+
+
+sub user_charts {
+	my ($self, $username) = @_;
+
+	#croak "Invalid username '$username'" if $username eq 'about';
+
+	my $url = "https://www.gittip.com/$username/charts.json";
+	return _get($url);
 }
 
 
@@ -76,8 +102,7 @@ Each element in the array has the following fields:
 
 sub paydays {
 	my $url = 'https://www.gittip.com/about/paydays.json';
-	my $charts = get $url;
-	return from_json $charts;
+	return _get($url);
 }
 
 =head2 stats
@@ -87,11 +112,52 @@ with lots of keys...
 
 =cut
 
+
+
 sub stats {
 	my $url = 'https://www.gittip.com/about/stats.json';
-	my $charts = get $url;
-	return from_json $charts;
+	return _get($url);
 }
+
+=head2 communities
+
+See L<https://github.com/gittip/www.gittip.com/issues/2014>
+
+L<https://www.gittip.com/for/perl/?limit=20&offset=20>
+
+L<https://github.com/gittip/www.gittip.com/issues/2408>
+
+Currently only returns an empty list.
+
+=cut
+
+sub communities {
+	my $url = 'https://www.gittip.com/for/communities.json';
+	return _get($url);
+}
+
+# https://www.gittip.com/about/tip-distribution.json
+# returns an array of numbers \d+\.\d\d  (over 8000 entries), probably the full list of tips.
+
+
+sub _get {
+	my ($url) = @_;
+
+	my $charts = get $url;
+	if (not defined $charts or $charts eq '') {
+		warn "Empty return\n";
+		return [];
+	}
+	my $data = eval { from_json $charts };
+	if ($@) {
+		warn $@;
+		warn "Data received: '$charts'\n";
+		$data = [];
+	}
+	return $data;
+}
+
+
 
 =head1 AUTHOR
 
